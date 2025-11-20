7fe9e541dd9b08db4de375fea1a65955a2d98c6f
Angelo Sanchez <jsanchezs17@upao.edu.pe>
2025-11-20 00:59:05 -0500

    cambios

--- lib/config.dart
+++ lib/config.dart
@@ -7,7 +7,6 @@ class Config {
   //static const String imagenesUrl = 'http://localhost:3000/api';
   //static const String wsUrl = 'ws://127.0.0.1:8000/ws';
 
-
   static const String apiUrl = 'https://funcyfnl.ddns.net/api';
   static const String apiUrl2 = 'https://funcyfnl.ddns.net/api';
   static const String imagenesUrl = 'https://funcyfnl.ddns.net/api';
