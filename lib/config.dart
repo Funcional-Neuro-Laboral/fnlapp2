288919ce456247d50263704111c23d9cc67bfbb6
Dantuloo <rabantom2@upao.edu.pe>
2026-02-06 11:04:02 -0500

    Fix: Actualización de endpoint a producción (puerto 3000)

--- lib/config.dart
+++ lib/config.dart
@@ -12,8 +12,8 @@ class Config {
   //static const String imagenesUrl = 'http://10.0.2.2:8000/api';
   //static const String wsUrl = 'ws://10.0.2.2:8000/ws';
 
-  static const String apiUrl = 'https://funcyfnl.ddns.net/api';
-  static const String apiUrl2 = 'https://funcyfnl.ddns.net/api';
-  static const String imagenesUrl = 'https://funcyfnl.ddns.net/api';
-  static const String wsUrl = 'wss://funcyfnl.ddns.net/ws';
+  static const String apiUrl = 'http://api.fnldigital.com:3000/api';
+  static const String apiUrl2 = 'http://api.fnldigital.com:3000/api';
+  static const String imagenesUrl = 'http://api.fnldigital.com:3000/api';
+  static const String wsUrl = 'ws://api.fnldigital.com:3000/ws';
 }
